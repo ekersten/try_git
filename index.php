8e6f29b6064068444c76bb5d82d38e8d5648579a
Eric Kersten <ekersten@gmail.com>
2012-10-25 16:00:26 -0300

    test page creation

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo php_info();
+<!-- this is a test page -->
+<!doctype html>
+<html>
+	<head>
+		<title>Test page</title>
+	</head>
+	<body>
+		<h1>Test page</h1>
+		<p>Sample text on the test page</p>
+		<p>Today is <?php echo date('m/d/Y'); ?></p>
+	</body>
+</html>
